207a3f7886943e96cdb0d3ebb986f64897401149
César Damasceno <64749161+ManeMaria@users.noreply.github.com>
2021-10-25 13:01:21 -0300

    novos ajustes

--- app.js
+++ app.js
@@ -3,26 +3,13 @@ import "./styles/index.css";
 import arrow from "./assets/images/arrow.png";
 import imgLogo from "./assets/images/logo_ntd.svg";
 
-function addHoverEvent() {
-  const navigation_menu = document.querySelectorAll("#navigation_menu a");
-  navigation_menu.forEach((ele) => {
-    ele.addEventListener("mouseover", (e) => {
-      const img = e.target.children[0];
-        img.style.visibility = "visible";
-    });
-    ele.addEventListener("mouseleave", (e) => {
-      const img = e.target.children[0];
-        img.style.visibility = "hidden";
-    });
-  });
-}
 const clickMenuMobile = () => {
   const default_button = document.querySelector("#default_button");
   const input = document.querySelector("#burguer_menu");
   const button = document.querySelector("#burguer_menu_container");
   const menu = document.querySelector("#menu");
   input.addEventListener("click", () => {
-    default_button.classList.toggle("font_full_effect");
+    
     menu.classList.toggle("show_menu");
     button.classList.toggle("translate_button");
   });
@@ -39,7 +26,6 @@ const addImg = () => {
 };
 
 document.addEventListener("DOMContentLoaded", () => {
-  addHoverEvent();
   clickMenuMobile();
   addImg();
 });
