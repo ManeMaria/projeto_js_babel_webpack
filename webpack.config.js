cc9391dbabbcfd235fa756d2c1a8bd37e48c7d15
César Damasceno <mirandacesar598@gmail.com>
2021-11-17 06:15:32 -0300

    correções nos pacotes webpack

--- webpack.config.js
+++ webpack.config.js
@@ -33,11 +33,7 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
-      },
-      {
-        test: /.svg$/,
-        loader: 'svg-inline-loader'
-      },
+      }
     ],
   },
   plugins: [
